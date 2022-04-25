7b2e9f6e229b62b8f14e685e03f84962e1c276a0
MrZorg111 <yodji333@mail.ru>
2022-04-25 22:16:08 +0300

    New solution

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+//Включение датчика switches_state |= HEATERS, отключение датчика switches_state &= ~HEATERS.
 
 enum switches
 {
@@ -6,10 +8,15 @@ enum switches
     LIGHTS_OUTSIDE = 2,
     HEATERS = 4,
     WATER_PIPE_HEATING = 8,
-    CONDITIONER = 16
+    CONDITIONER = 16,
+    OUTLETS = 32
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+   bool power_supply, moving;
+   std::stringstream readings;
+   std::string lights_inside, lights_outside, heaters, outlets, water_heating, conditioner;
+   std::cout << "Enter the readings of all sensors:";
+   std::cin >> readings;
     return 0;
 }
